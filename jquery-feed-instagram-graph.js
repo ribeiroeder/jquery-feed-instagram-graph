e66553b97b4d287dc861eb28b81ee6550f8d5e44
ribeiroeder <contato@ederribeiro.net>
2021-04-30 22:04:08 -0300

    Add files via upload

---
+++ jquery-feed-instagram-graph.js
@@ -0,0 +1,39 @@
+// jQuery Ajax for feed Instagram Graph API
+if ( $('#instagram-feed1').length != 0 ) {
+
+    var token = 'IGQVJVYllVTGUwNk5GUDlfbklrVnFhWlgwMTNHaTNZAQUNzVHo1WVJFSVdaZAlNQTUxENUl3dlhlMVVLdGJZAbHducVdmd1VvMmoydWs1RFFjZAWNtTDUzUmM2cHZAaZAEVfOVlZAN1NpLUpPakFET3dWaWJPSwZDZD';
+    var fields = 'id,media_type,media_url,thumbnail_url,timestamp,permalink,caption';
+    var limit = 6;
+
+    $.ajax ( {
+        url: 'https://graph.instagram.com/me/media?fields='+ fields +'&access_token='+ token +'&limit='+ limit,
+        type: 'GET',
+        success: function( response ) {
+            for( var x in response.data ) {
+                var link = response.data[x]['permalink'],
+                    caption = response.data[x]['caption'],
+                    image = response.data[x]['media_url'],
+                    image_video = response.data[x]['thumbnail_url'],
+                    html = '';
+                if ( response.data[x]['media_type'] == 'VIDEO' ) {  
+                    html += '<div class="instagram_new">';
+                        html += '<a class="insta-link" href="' + link + '" rel="noopener" target="_blank">';
+                            html += '<img src="' + image_video + '" loading="lazy" alt="' + caption + '" class="insta-image" />';
+                        html += '</a>';
+                    html += '</div>';
+                } else {
+                    html += '<div class="instagram_new">';
+                        html += '<a class="insta-link" href="' + link + '" rel="noopener" target="_blank">';
+                            html += '<img src="' + image + '" loading="lazy" alt="' + caption + '" class="insta-image" />';
+                        html += '</a>';
+                    html += '</div>';
+                }
+                $('#instagram-feed1').append(html);
+            }
+        },
+        error: function(data) { 
+            var html = '<div class="p-10px-tb">No Images Found</div>'; 
+            $('#instagram-feed1').append(html); 
+            }
+    });
+}
